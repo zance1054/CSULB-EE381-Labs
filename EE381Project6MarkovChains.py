d91b9b96ea38f2c7f625494edbdcacaeff0f37ea
Alec <32371145+zance1054@users.noreply.github.com>
2018-04-26 14:27:24 -0700

    Problem 1
    Finished problem 1 with figure showing transition between states

--- EE381Project6MarkovChains.py
+++ EE381Project6MarkovChains.py
@@ -1,53 +1,72 @@
-#CHAPTER 11.
-#MARKOV CHAINS
-#state.
-#The probabilities are called transition probabilities.
-#The process can remainin the state it is in, and this occurs with probabilitypii.
-#An initial probabilitydistribution, defined onS, specifies the starting state.
-#Usually this is done byspecifying a particular state as the starting state.
-#R. A. Howard1 provides us with a picturesque description of a Markov chain asa frog jumping on a set of lily pads.
-#The frog starts on one of the pads and thenjumps from #lily pad to lily pad with the appropriate transition probabilities.
-#Example 11.1 According to Kemeny, Snell, and Thompson,
-#2 the Land of Oz is blessed by many things, but not by good weather.
-#They never have two nice days in a row.
-#If they have a nice day, they are just as likely to have snow as rain the next day.
-#If they have snow or rain, they have an even chance of having the samethe next day.
-#If there is change from snow or rain, only half of the time is this achange to a nice day.
-#With this #information we form a Markov chain as follows
-#We take as states the kinds of weather R, N, and S.
-#From the above informationwe determine the transition probabilities.
-#These are most conveniently representedin a square array as
-#P=R    N    SR
-#   1/2  1/4  1/4N
-#   1/2   0   1/2S
-#   1/4  1/4  1/2
-
-#most occuring / solution should be B
+#most occuring / solution should be B for number 2
 
 #EE381 Alexander Fielding
 # Project 6 Markov Chain
 
-from pymarkovchain import MarkovChain
 import numpy as np
+from matplotlib import pylab as pyplt
+from matplotlib.legend_handler import HandlerLine2D
 
-n = 15 # time steps
+n = 16 # time steps + 1
 N = 10000 # number simulations
+X = []
+a = 0
+b = 1
+nbins = 50
+
 
 #mc = MarkovChain("text")
 
 states = ['R','N','S']
 initialprobs = [0.25,0.5,0.25]
-#mat = np.matrix([(1/3),(1/3),(1/3)],[0.5,0,0.5],[0.25,0.25,0.5])
+stateR = [(1/3),0.5,0.25]
+stateN = [(1/3),0.5,0.25]
+stateS = [(1/3),0.5,0.25]
+probR = []
+probN = []
+probS = []
 
 def detInitialState():
     initialstate = np.random.choice(states,1,p = initialprobs)
 
     return initialstate
 
+def nextState(probs):
+    nextState = np.random.choice(states,1,probs)
+    return nextState
+
+def detprobability():
+    return p
 #Main
 count = 0
-while(count < N):
+state = detInitialState()
+X.append(state)
+nums = []
+while(count < n):
+    nums.append(count)
     count += 1
-    state = detInitialState()
+    if(state == 'R'):
+        state = nextState(stateR)
+        probR.append(1)
+        probN.append(0)
+        probS.append(0)
+    elif(state == 'N'):
+        state = nextState(stateN)
+        probN.append(1)
+        probS.append(0)
+        probR.append(0)
+    elif(state == 'S'):
+        state = nextState(stateS)
+        probS.append(1)
+        probR.append(0)
+        probN.append(0)
+    X.append(state)
 
-    print(state)
+line1, = pyplt.plot(nums,probR,linestyle='--',label='R')
+line2, = pyplt.plot(nums,probS,linestyle='--',label='S')
+line3, = pyplt.plot(nums,probN,linestyle='--',label='N')
+pyplt.legend(handler_map={line1: HandlerLine2D(numpoints=4)})
+pyplt.title('Three-State Markov Chain Result from 10,000 simulations')
+pyplt.ylabel('Probability')
+pyplt.xlabel('Step Number')
+pyplt.show()
